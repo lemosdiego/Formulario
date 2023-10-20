64da50b4205028519181aadafa6b172f4c3c77ed
Washington <lemos.diego1910@gmail.com>
2023-10-20 16:36:45 -0300

    Tela decadastro

---
+++ src/app/componentes/Cadastro/index.tsx
@@ -0,0 +1,30 @@
+import { Button, Checkbox, FormControlLabel, Grid, Link, OutlinedInput, Typography } from "@mui/material"
+
+
+export const Cadastro = () => {
+  return (
+    <Grid container display="flex" justifyContent="center" height="100vh" alignContent="center">
+      <Grid >
+        <Typography variant="h5" sx={{paddingLeft:"45px", paddingTop:"20px", color:"blue"}}>Preencher Cadastro</Typography>
+        <Grid display="flex" justifyContent="center" gap="10px" paddingTop="30px">
+          <OutlinedInput sx={{ width: "195px" }}></OutlinedInput>
+          <OutlinedInput sx={{ width: "195px" }}></OutlinedInput>
+        </Grid>
+        <Grid display="flex" flexDirection="column" alignItems="center" gap="10px" paddingTop="10px">
+          <OutlinedInput sx={{ width: "400px" }}></OutlinedInput>
+          <OutlinedInput sx={{ width: "400px" }}></OutlinedInput>
+          <OutlinedInput sx={{ width: "400px" }}></OutlinedInput>
+        </Grid>
+        <Grid display="flex" width="400px" gap="10px" paddingTop="10px"paddingLeft="22px">
+        <FormControlLabel control={<Checkbox defaultChecked />} label="Quero receber atualizações via E-mail" />
+        </Grid>
+        <Grid display="flex" width="100%" gap="10px" paddingTop="10px" alignItems="center">
+        <Button variant="contained" sx={{width:"400px", height:"50px"}}>Inscrever-se</Button>
+        </Grid>
+        <Grid display="flex" width="100%" gap="10px" paddingTop="20px">
+          <Link>Já tem uma conta? Entrar.</Link>
+        </Grid>
+      </Grid>
+    </Grid>
+  )
+}
