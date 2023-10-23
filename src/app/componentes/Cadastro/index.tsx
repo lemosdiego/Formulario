1f7d9aad9d3c0317faca2a5fe9d88552c33b808c
diego.lemos <57995631+lemosdiego@users.noreply.github.com>
2023-10-23 09:45:10 -0300

    Placeholder

--- src/app/componentes/Cadastro/index.tsx
+++ src/app/componentes/Cadastro/index.tsx
@@ -7,13 +7,14 @@ export const Cadastro = () => {
       <Grid >
         <Typography variant="h5" sx={{paddingLeft:"45px", paddingTop:"20px", color:"blue"}}>Preencher Cadastro</Typography>
         <Grid display="flex" justifyContent="center" gap="10px" paddingTop="30px">
-          <OutlinedInput sx={{ width: "195px" }}></OutlinedInput>
-          <OutlinedInput sx={{ width: "195px" }}></OutlinedInput>
+          <OutlinedInput placeholder="Nome" sx={{ width: "195px" }}></OutlinedInput>
+          <OutlinedInput placeholder="Sobrenome" sx={{ width: "195px" }}></OutlinedInput>
         </Grid>
         <Grid display="flex" flexDirection="column" alignItems="center" gap="10px" paddingTop="10px">
-          <OutlinedInput sx={{ width: "400px" }}></OutlinedInput>
-          <OutlinedInput sx={{ width: "400px" }}></OutlinedInput>
-          <OutlinedInput sx={{ width: "400px" }}></OutlinedInput>
+          <OutlinedInput placeholder="Email" sx={{ width: "400px" }}></OutlinedInput>
+          <OutlinedInput placeholder="Confirm Email" sx={{ width: "400px" }}></OutlinedInput>
+          <OutlinedInput placeholder="Senha" sx={{ width: "400px" }}></OutlinedInput>
+          <OutlinedInput placeholder="Confirm Senha" sx={{ width: "400px" }}></OutlinedInput>
         </Grid>
         <Grid display="flex" width="400px" gap="10px" paddingTop="10px"paddingLeft="22px">
         <FormControlLabel control={<Checkbox defaultChecked />} label="Quero receber atualizações via E-mail" />
