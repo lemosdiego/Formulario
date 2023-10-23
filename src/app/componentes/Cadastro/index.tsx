1fedd07399746d33adb8d44c510f62c7dc9371c5
diego.lemos <57995631+lemosdiego@users.noreply.github.com>
2023-10-23 10:44:02 -0300

    Atualização de icone

--- src/app/componentes/Cadastro/index.tsx
+++ src/app/componentes/Cadastro/index.tsx
@@ -1,11 +1,17 @@
-import { Button, Checkbox, FormControlLabel, Grid, Link, OutlinedInput, Typography } from "@mui/material"
+import { Avatar, Button, Checkbox, FormControlLabel, Grid, Link, OutlinedInput, Typography } from "@mui/material"
+import Image from "next/image"
 
 
 export const Cadastro = () => {
   return (
-    <Grid container display="flex" justifyContent="center" height="100vh" alignContent="center">
-      <Grid >
-        <Typography variant="h5" sx={{paddingLeft:"45px", paddingTop:"20px", color:"blue"}}>Preencher Cadastro</Typography>
+    <Grid container display="flex" justifyContent="center" height="100vh" alignContent="center" flexDirection="column" alignItems="center" gap="4px">
+      <Grid item height="50px" width="50px" borderRadius={50}>
+       <img src="/usuario.svg" height="100%" width={"100%"} />
+      </Grid>
+      <Grid item>
+      <Typography variant="h6" >Cadastro</Typography>
+      </Grid>
+      <Grid>
         <Grid display="flex" justifyContent="center" gap="10px" paddingTop="30px">
           <OutlinedInput placeholder="Nome" sx={{ width: "195px" }}></OutlinedInput>
           <OutlinedInput placeholder="Sobrenome" sx={{ width: "195px" }}></OutlinedInput>
